decbaa8be9f56d61e1882a5823333b7d6ae2102f
Shaina Mae Mainar <mainarsa@cofc.edu>
2019-09-29 01:37:01 -0400

    changes

--- example/iris-data-classifier/ML-React-App-Template/service/app.py
+++ example/iris-data-classifier/ML-React-App-Template/service/app.py
@@ -1,32 +1,33 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
 from sklearn.externals import joblib
-import numpy as np
-import sys
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app, 
 		  version = "1.0", 
-		  title = "Iris Plant identifier", 
-		  description = "Predict the type of iris plant")
+		  title = "ML React App", 
+		  description = "Predict results using a trained model")
 
 name_space = app.namespace('prediction', description='Prediction APIs')
 
 model = app.model('Prediction params', 
-				  {'sepalLength': fields.Float(required = True, 
-				  							   description="Sepal Length", 
-    					  				 	   help="Sepal Length cannot be blank"),
-				  'sepalWidth': fields.Float(required = True, 
-				  							   description="Sepal Width", 
-    					  				 	   help="Sepal Width cannot be blank"),
-				  'petalLength': fields.Float(required = True, 
-				  							description="Petal Length", 
-    					  				 	help="Petal Length cannot be blank"),
-				  'petalWidth': fields.Float(required = True, 
-				  							description="Petal Width", 
-    					  				 	help="Petal Width cannot be blank")})
+				  {'textField1': fields.String(required = True, 
+				  							   description="Text Field 1", 
+    					  				 	   help="Text Field 1 cannot be blank"),
+				  'textField2': fields.String(required = True, 
+				  							   description="Text Field 2", 
+    					  				 	   help="Text Field 2 cannot be blank"),
+				  'select1': fields.Integer(required = True, 
+				  							description="Select 1", 
+    					  				 	help="Select 1 cannot be blank"),
+				  'select2': fields.Integer(required = True, 
+				  							description="Select 2", 
+    					  				 	help="Select 2 cannot be blank"),
+				  'select3': fields.Integer(required = True, 
+				  							description="Select 3", 
+    					  				 	help="Select 3 cannot be blank")})
 
-classifier = joblib.load('classifier.joblib')
+# classifier = joblib.load('classifier.joblib')
 
 @name_space.route("/")
 class MainClass(Resource):
@@ -43,12 +44,11 @@ class MainClass(Resource):
 		try: 
 			formData = request.json
 			data = [val for val in formData.values()]
-			prediction = classifier.predict(np.array(data).reshape(1, -1))
-			types = { 0: "Iris Setosa", 1: "Iris Versicolour ", 2: "Iris Virginica"}
+			# prediction = classifier.predict(data)
 			response = jsonify({
 				"statusCode": 200,
 				"status": "Prediction made",
-				"result": "The type of iris plant is: " + types[prediction[0]]
+				"result": "Prediction: " + str(data)
 				})
 			response.headers.add('Access-Control-Allow-Origin', '*')
 			return response
